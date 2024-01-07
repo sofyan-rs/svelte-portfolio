cfa6e5c5d86019d657b69504678b1dc10c30af14
Sofyan R <sofyan423@gmail.com>
2024-01-07 11:21:07 +0700

    add meta description

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -46,7 +46,7 @@
 <header class:scroll={isScrolled} class="header fixed z-10 top-0 left-0 right-0 backdrop-blur-md">
   <div class='container mx-auto p-5 text-gray-200 flex justify-between items-center'>
     <CarBatteryIcon className='animate-pulse' />
-    <button class="xl:hidden animate-pulse" on:click={() => showMenu = !showMenu}>
+    <button name='show-menu' class="xl:hidden animate-pulse" on:click={() => showMenu = !showMenu}>
       <ChevronsDown size={32} class={`text-gray-200 transition-all duration-300 ${showMenu && 'transform rotate-180'}`} />
     </button>
     <div class='menu absolute top-[72px] left-0 right-0 bg-gray-900/90 flex flex-col items-start lg:bg-inherit lg:top-0 lg:w-auto lg:gap-5 lg:flex-row lg:relative' class:show={showMenu}>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <svelte:head>
-  <title>Sofyan RS - My Portofolio</title>
+  <title>Sofyan RS - My Portfolio</title>
+  <meta name="description" content="Sofyan Rudiana Syamsi - Portfolio" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin='true'>
   <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700&display=swap" rel="stylesheet">
